7426e0afe03af71b805c07c7169900e1f7ba8566
Anton Fougner <fougneranton@gmail.com>
2022-10-27 12:41:29 +0200

    added "KOLLA VAD JAG KAN SKRIVA"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //KOLLA VAD JAG KAN SKRIVA
+        //KOLLA VAD JAG KAN SKRIVA!
     }
 }
